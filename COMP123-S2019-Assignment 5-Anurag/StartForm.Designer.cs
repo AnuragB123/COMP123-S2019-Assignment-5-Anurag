7aac3808ef8ee02cb172593d561a2eeb707b9dd9
Anurag Bhattacharya <abhatt66@my.centennialcollege.ca>
2019-07-31 21:16:02 -0400

    Created Start Form with specific design as per requirements

---
+++ COMP123-S2019-Assignment 5-Anurag/StartForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace COMP123_S2019_Assignment_5_Anurag
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
+            this.OrderLabel = new System.Windows.Forms.Label();
+            this.IconPictureBox = new System.Windows.Forms.PictureBox();
+            this.StartNewOrderButton = new System.Windows.Forms.Button();
+            this.OpenSavedOrderButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.IconPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OrderLabel
+            // 
+            this.OrderLabel.Location = new System.Drawing.Point(429, 9);
+            this.OrderLabel.Name = "OrderLabel";
+            this.OrderLabel.Size = new System.Drawing.Size(547, 76);
+            this.OrderLabel.TabIndex = 0;
+            this.OrderLabel.Text = "Order Your Computer Today!";
+            this.OrderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IconPictureBox
+            // 
+            this.IconPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("IconPictureBox.Image")));
+            this.IconPictureBox.Location = new System.Drawing.Point(579, 88);
+            this.IconPictureBox.Name = "IconPictureBox";
+            this.IconPictureBox.Size = new System.Drawing.Size(261, 268);
+            this.IconPictureBox.TabIndex = 1;
+            this.IconPictureBox.TabStop = false;
+            // 
+            // StartNewOrderButton
+            // 
+            this.StartNewOrderButton.Location = new System.Drawing.Point(551, 362);
+            this.StartNewOrderButton.Name = "StartNewOrderButton";
+            this.StartNewOrderButton.Size = new System.Drawing.Size(347, 94);
+            this.StartNewOrderButton.TabIndex = 2;
+            this.StartNewOrderButton.Text = "Start a New Order";
+            this.StartNewOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // OpenSavedOrderButton
+            // 
+            this.OpenSavedOrderButton.Location = new System.Drawing.Point(551, 497);
+            this.OpenSavedOrderButton.Name = "OpenSavedOrderButton";
+            this.OpenSavedOrderButton.Size = new System.Drawing.Size(347, 94);
+            this.OpenSavedOrderButton.TabIndex = 3;
+            this.OpenSavedOrderButton.Text = "Open a Saved Order";
+            this.OpenSavedOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(551, 623);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(347, 94);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 741);
+            this.ControlBox = false;
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.OpenSavedOrderButton);
+            this.Controls.Add(this.StartNewOrderButton);
+            this.Controls.Add(this.IconPictureBox);
+            this.Controls.Add(this.OrderLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome to Dollar Computer!";
+            ((System.ComponentModel.ISupportInitialize)(this.IconPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OrderLabel;
+        private System.Windows.Forms.PictureBox IconPictureBox;
+        private System.Windows.Forms.Button StartNewOrderButton;
+        private System.Windows.Forms.Button OpenSavedOrderButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
