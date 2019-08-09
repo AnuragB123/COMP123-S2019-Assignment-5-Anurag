facdc9756a7f7e1e8653a6ced0887cf4365f935b
Anurag Bhattacharya <abhatt66@my.centennialcollege.ca>
2019-08-09 16:11:52 -0400

    Open File Programmed

--- COMP123-S2019-Assignment 5-Anurag/Views/ProductInfoForm.cs
+++ COMP123-S2019-Assignment 5-Anurag/Views/ProductInfoForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -134,6 +135,71 @@ namespace COMP123_S2019_Assignment_5_Anurag
                     //this is a "modal" form
                     MessageBox.Show("File Saved...", "Saving File...",
                         MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //configure the file dialog
+            ProductOpenFileDialog.FileName = "Student.txt";
+            ProductOpenFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            ProductOpenFileDialog.Filter = "Text Files (*.txt)|*.txt| All Files (*.*)|*.*";
+
+            // open the file dialog
+            var result = ProductOpenFileDialog.ShowDialog();
+            if(result != DialogResult.Cancel)
+            {
+                try
+                {
+                    // Open the stream for reading
+                    using (StreamReader inputStream = new StreamReader(
+                        File.Open(ProductOpenFileDialog.FileName, FileMode.Open)))
+                    {
+                        //read from the file
+                        Program.product.productID = short.Parse(inputStream.ReadLine());
+                        Program.product.cost = decimal.Parse(inputStream.ReadLine());
+                        Program.product.manufacturer = inputStream.ReadLine();
+                        Program.product.model = inputStream.ReadLine();
+                        Program.product.RAM_type = inputStream.ReadLine();
+                        Program.product.RAM_size = inputStream.ReadLine();
+                        Program.product.displaytype = inputStream.ReadLine();
+                        Program.product.screensize = inputStream.ReadLine();
+                        Program.product.resolution = inputStream.ReadLine();
+                        Program.product.CPU_Class = inputStream.ReadLine();
+                        Program.product.CPU_brand = inputStream.ReadLine();
+                        Program.product.CPU_type = inputStream.ReadLine();
+                        Program.product.CPU_speed = inputStream.ReadLine();
+                        Program.product.CPU_number = inputStream.ReadLine();
+                        Program.product.condition = inputStream.ReadLine();
+                        Program.product.OS = inputStream.ReadLine();
+                       `Program.product.platform = inputStream.ReadLine();
+                        Program.product.HDD_size = inputStream.ReadLine();
+                        Program.product.HDD_speed = inputStream.ReadLine();
+                        Program.product.CPU_type = inputStream.ReadLine();
+                        Program.product.optical_drive = inputStream.ReadLine();
+                        Program.product.Audio_type = inputStream.ReadLine();
+                        Program.product.LAN = inputStream.ReadLine();
+                        Program.product.WIFI = inputStream.ReadLine();
+                        Program.product.width = inputStream.ReadLine();
+                        Program.product.height = inputStream.ReadLine();
+                        Program.product.depth = inputStream.ReadLine();
+                        Program.product.weight = inputStream.ReadLine();
+                        Program.product.moust_type = inputStream.ReadLine();
+                        Program.product.power = inputStream.ReadLine();
+                        Program.product.webcam = inputStream.ReadLine();
+
+                        //cleanup
+                        inputStream.Close();
+                        inputStream.Dispose();
+                    }
+
+                    NextButton_Click(sender, e);
+                }
+                catch (IOException exception)
+                {
+                    Debug.WriteLine("ERROR: " + exception.Message);
+
                 }
             }
         }
